a115b234c9636ba21d95f2c66effbaef6e189810
Gra <gracielemarcelinosoares18@gmail.com>
2026-08-20 22:07:49 -0300

    Update robo.py

--- robo.py
+++ robo.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -6,12 +7,9 @@ app = Flask(__name__)
 def receber_mensagem():
     dados = request.json
     print("Nova mensagem do cliente:", dados)
-    
-    # É aqui que vamos colocar a lógica para responder automaticamente 
-    # com o cardápio de açaí e chocolate quente do Delivery Jubileu!
-    
     return "OK", 200
 
 if __name__ == "__main__":
-    print("Robô do Jubileu rodando na porta 5000...")
-    app.run(port=5000)
+    # O Render exige que pegemos a porta dinâmica dele
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=porta)
